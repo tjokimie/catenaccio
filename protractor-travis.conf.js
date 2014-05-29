6879f5615ffe72f7e9160fa791b5bf6c2ed59b1b
Tuomo Jokimies <tuomo.jokimies@gmail.com>
2014-05-29 17:56:49 +0300

    Increase selenium browserTimeout

--- protractor-travis.conf.js
+++ protractor-travis.conf.js
@@ -1,6 +1,7 @@
 var config = require('./protractor-shared.conf').config;
 
 config.allScriptsTimeout = 30000;
+config.seleniumArgs = [ '-browserTimeout=60' ];
 
 config.capabilities = {
     browserName: 'phantomjs'
