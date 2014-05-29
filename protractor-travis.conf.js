31665e105685364f6ef8ea2638c39f86f9381dac
Tuomo Jokimies <tuomo.jokimies@gmail.com>
2014-05-29 18:11:55 +0300

    Use karma phantomjs in travis e2e-tests

--- protractor-travis.conf.js
+++ protractor-travis.conf.js
@@ -4,7 +4,8 @@ config.allScriptsTimeout = 30000;
 config.seleniumArgs = [ '-browserTimeout=60' ];
 
 config.capabilities = {
-    browserName: 'phantomjs'
+    browserName: 'phantomjs',
+    'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs'
 };
 
 exports.config = config;
