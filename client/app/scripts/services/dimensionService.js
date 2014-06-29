caf8409cb57876e83b56ab1f1a741f2cb5eb1e5e
Tuomo Jokimies <tuomo.jokimies@gmail.com>
2014-06-29 14:47:12 +0300

    Dimensions based on body margins

--- client/app/scripts/services/dimensionService.js
+++ client/app/scripts/services/dimensionService.js
@@ -1,15 +1,19 @@
 angular.module('catenaccio.services')
-    .factory('dimensionService', function ($window) {
+    .factory('dimensionService', function ($document, $window) {
         'use strict';
 
         var dimensionService = {};
 
         dimensionService.width = function () {
-            return $($window).width() * 0.8;
+            var marginLeft = $($document[0].body).css('margin-left');
+            var marginRight = $($document[0].body).css('margin-right');
+            return $($window).width() - parseInt(marginLeft, 10) - parseInt(marginRight, 10);
         };
 
         dimensionService.height = function () {
-            return $($window).height() * 0.8;
+            var marginTop = $($document[0].body).css('margin-top');
+            var marginBottom = $($document[0].body).css('margin-bottom');
+            return $($window).height() - parseInt(marginTop, 10) - parseInt(marginBottom, 10);
         };
 
         return dimensionService;
--- client/test/spec/services/dimensionService.js
+++ client/test/spec/services/dimensionService.js
@@ -13,11 +13,13 @@ describe('Service: dimensionService', function () {
 
     it('should calculate width', function () {
         spyOn(jQuery.fn, 'width').andReturn(1000);
+        spyOn(jQuery.fn, 'css').andReturn('100px');
         expect(dimensionService.width()).toBe(800);
     });
 
     it('should calculate height', function () {
         spyOn(jQuery.fn, 'height').andReturn(500);
+        spyOn(jQuery.fn, 'css').andReturn('50px');
         expect(dimensionService.height()).toBe(400);
     });
 });
