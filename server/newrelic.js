a9d925b0a42df929b35af0c4be047d7ceb68c381
Tuomo Jokimies <tuomo.jokimies@gmail.com>
2014-06-29 13:56:24 +0300

    Add New Relic app monitoring

---
+++ server/newrelic.js
@@ -0,0 +1,10 @@
+exports.config = {
+
+    /* jshint camelcase: false */
+    app_name: ['Catenaccio'],
+    /* jshint camelcase: true */
+
+    logging: {
+        level: 'info'
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('newrelic');
+
 var app = require('./app/app');
 
 module.exports = app.listen(app.get('port'), function() {
