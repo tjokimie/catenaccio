6a39269a7002e39021b2c8c85647c0554898661c
Tuomo Jokimies <tuomo.jokimies@gmail.com>
2014-06-29 14:05:55 +0300

    Increase logging

--- server/app/controllers/tactics.js
+++ server/app/controllers/tactics.js
@@ -7,6 +7,7 @@ exports.create = function (req, res) {
     var tactics = new Tactics(req.body);
     tactics.save(function (err, data) {
         if (err) {
+            console.log(req.headers, req.body, err);
             res.send(400);
         } else {
             res.location('/api/tactics/' + data._id);
@@ -18,6 +19,9 @@ exports.create = function (req, res) {
 exports.findOne = function (req, res) {
     Tactics.findOne({ _id: req.params.id }, function (err, data) {
         if (err || !data) {
+            if (err) {
+                console.log(req.headers, req.body, err);
+            }
             res.send(404);
         } else {
             res.send(200, data.getPublicFields());
